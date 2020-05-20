f871ac8588011d0c9f40f00aa501a3227ab30422
Giang Truong <giang@spiderbox.design>
2020-05-20 22:12:56 +0700

    Change to Reactive Form

--- ProductAngular/src/app/product-dialog/product-dialog.component.ts
+++ ProductAngular/src/app/product-dialog/product-dialog.component.ts
@@ -3,16 +3,17 @@ import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import { DialogData } from "../dialog-data";
 import { Product } from "../product";
 import { ProductService } from "../product.service";
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-product-dialog",
   templateUrl: "./product-dialog.component.html",
-  styleUrls: ["./product-dialog.component.css"]
+  styleUrls: ["./product-dialog.component.css"],
 })
 export class ProductDialogComponent implements OnInit {
   product: Product;
-  name: string;
-  price: number;
+  name = new FormControl("");
+  price = new FormControl(0.0);
   mode: string;
   modal_title: string;
 
@@ -26,11 +27,14 @@ export class ProductDialogComponent implements OnInit {
     const { product, mode } = this.data;
     this.product = product;
     this.mode = mode;
-    this.name = this.product ? this.product.name : "";
-    this.price = this.product ? this.product.price : null;
     this.modal_title = this.product
       ? `Edit ${this.product.name}`
       : "New product";
+
+    if (this.product) {
+      this.name.setValue(this.product.name);
+      this.price.setValue(this.product.price);
+    }
   }
   onNoClick(): void {
     this.dialogRef.close();
@@ -39,18 +43,22 @@ export class ProductDialogComponent implements OnInit {
     this.product && this.product.id ? this.updateProduct() : this.addProduct();
   }
   addProduct(): void {
-    const { name, price } = this;
-
-    this.productService.addProduct({ name, price } as Product).subscribe(_ => {
-      this.dialogRef.close();
-      this.data.afterSave();
-    });
+    const name = this.name.value;
+    const price = this.price.value;
+    this.productService
+      .addProduct({ name, price } as Product)
+      .subscribe(() => this.afterSave());
   }
   updateProduct(): void {
-    this.product = { ...this.product, name: this.name, price: this.price };
-    this.productService.updateProduct(this.product).subscribe(() => {
-      this.dialogRef.close();
-      this.data.afterSave();
-    });
+    const name = this.name.value;
+    const price = this.price.value;
+    this.product = { ...this.product, name: name, price: price };
+    this.productService
+      .updateProduct(this.product)
+      .subscribe(() => this.afterSave());
+  }
+  afterSave(): void {
+    this.dialogRef.close();
+    this.data.afterSave();
   }
 }
